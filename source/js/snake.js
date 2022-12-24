9bf907da75e972fc77db3568ffa0f54bc71c2223
ElkinAndrey <andreyridbir@gmail.com>
2022-12-24 12:24:15 +0500

    Добавление главного цикла игры

---
+++ source/js/snake.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('snakeGame');
+var context = canvas.getContext('2d');
+
+// игровой цикл
+function loop() {
+    requestAnimationFrame(loop);
+
+    
+}
+
+
+// начать игру
+requestAnimationFrame(loop);
