2dac1600a32187564027cde85a10aaccf893d6dd
Denis Melnikov <sinennn@yandex.ru>
2022-12-24 18:41:27 +0500

    Написан метод, который получает get запрос и отсылает назад таблицу лидеров

--- source/js/dbRouter.js
+++ source/js/dbRouter.js
@@ -10,5 +10,13 @@ router.post('/', async (req, res)=>{
             res.status(500).end(e.message);
       }     
 });
+router.get('/', async (req, res)=>{
+      try{
+            var leaderboard = await snakeResult.find({}).sort({points: -1}).limit(20);      
+            res.status(200).json(leaderboard);  
+      }catch(e){
+            res.status(500).json(e.message);
+      }   
+});
 module.exports = router;
 return router;
